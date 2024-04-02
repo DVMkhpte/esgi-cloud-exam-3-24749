8451f62657287cad417b895d330051bca1ea99c5
DVM_khpte <147807946+DVMkhpte@users.noreply.github.com>
2024-04-02 12:38:43 +0200

    Add File

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'Y832WAUKG8EWUIHEE05D', // TODO
+    secretAccessKey: 'uvKwWDdKhFU6GPsFihFCbzUu5jROTjD0z6ZReyVX', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-24749', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
